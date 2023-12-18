196c93d5ddf31f2fda5fa884f7d2af4795720303
Cyril <cyril.personne@outlook.fr>
2023-12-18 18:02:56 +0100

    admin login

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminController extends AbstractController
+{
+    #[Route(path: '/admin/login', name: 'app_admin_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['username' => '$lastUsername', 'error' => $error]);
+    }
+
+    #[Route(path: '/admin/logout', name: 'app_admin_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/admin/dashboard', name: 'app_admin_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
