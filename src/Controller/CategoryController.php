f220d03184abbf579b303017d0e5185cdcfb0f74
sethuitneuf <huitgraphisme@gmail.com>
2023-12-19 17:17:07 +0100

    save

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(): Response
+    {
+        return $this->render('category.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+}
