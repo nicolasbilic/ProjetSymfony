554f9ac35947f06cad8ea9c0149dc590f920acd5
Max182xx <m.garciapro34@gmail.com>
2023-12-19 13:39:50 +0100

    save

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends AbstractController
+class ItemController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
+    #[Route('/item', name: 'app_item')]
     public function index(): Response
     {
-        return $this->render('contact.html.twig', [
-            'controller_name' => 'ContactController',
+        return $this->render('item.html.twig', [
+            'controller_name' => 'ItemController',
         ]);
     }
 }
