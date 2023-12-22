6f865a7dcd3312aa561ca1552f304de21fbe0c52
zKittyGB <a-hebert@hotmail.fr>
2023-12-22 14:20:48 +0100

    switch json data to sql

--- src/Controller/ProductUserController.php
+++ src/Controller/ProductUserController.php
@@ -15,13 +15,11 @@ use App\Entity\Product;
 
 class ProductUserController extends AbstractController
 {
-    private $kernel;
     private $productRepos;
     private $errors;
 
-    public function __construct(KernelInterface $kernel, ProductRepository $productRepos)
+    public function __construct(ProductRepository $productRepos)
     {
-        $this->kernel = $kernel;
         $this->productRepos = $productRepos;
         $this->errors;
     }
@@ -58,12 +56,7 @@ class ProductUserController extends AbstractController
             $this->errors = "No product found.";
         }
 
-        // Get additional data from the JSON file
-        $jsonData = $this->getJsonData('src/data/productUserData.json');
-        $data = json_decode($jsonData, true);
-
         return $this->render('productUser/productUser.html.twig', [
-            'data' => $data,
             'product' => $product,
             'errors' => $this->errors,
             'form' => $form->createView(),
@@ -73,19 +66,5 @@ class ProductUserController extends AbstractController
     private function getProduct($productId)
     {
         return $this->productRepos->find($productId);
-    }
-
-    //Method to get the data's JSON
-    private function getJsonData(string $path): string
-    {
-        $jsonFilePath = $this->kernel->getProjectDir() . '/' . $path;
-
-        //Check if the JSON file exists
-        if (!file_exists($jsonFilePath)) {
-            $this->errors[] = "JSON file not found at '{$jsonFilePath}'.";
-            return '';
-        }
-
-        return file_get_contents($jsonFilePath);
     }
 }
