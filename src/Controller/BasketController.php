464df3201898a709ace81c19dcd850847cb74cdc
zKittyGB <a-hebert@hotmail.fr>
2023-12-23 13:39:25 +0100

    check if product exist in cart then add quantity

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Form\CartType;
+use App\Repository\ProductRepository;
 use App\Services\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Services\UserManager;
@@ -22,49 +22,21 @@ class BasketController extends AbstractController
         $this->requestStack = $requestStack;
     }
 
-    public function displayBasket(UserManager $userManager, CartService $cartService, Request $request, EntityManagerInterface $em): Response
+    public function displayBasket(UserManager $userManager, CartService $cartService): Response
     {
-        $form = $this->createForm(CartType::class);
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted()) {
-            //Get user
-            $user = $this->getUser();
-            // Vérifiez si l'utilisateur est connecté
-            if ($user) {
-                $product = $em->getRepository(Product::class)->find(17);
-
-                dump($product);
-
-                $cartService->addProductToCart($user, $product, 1);
-            }
-        }
 
         $this->loadUserBasket($cartService);
-
+        $products = $cartService->getCartList();
         return $this->render(
             'basketUser/basket.html.twig',
             [
-                'productList' => [
-                    [
-                        "picture" => "hache.png",
-                        "name" => "Hache de combat pour truçidage zombien",
-                        "price" => 179,
-                        "quantity" => 1,
-
-                    ], [
-                        "picture" => "hache.png",
-                        "name" => "Hache de combat pour truçidage zombien",
-                        "price" => 179,
-                        "quantity" => 1,
-                    ],
-                ],
                 "totalePrice" => 388,
+                "products" => $products,
                 'isLoggedIn' => $userManager->isLoggedIn,
             ]
         );
     }
-
 
     public function loadUserBasket(CartService $cartService): void
     {
@@ -74,26 +46,23 @@ class BasketController extends AbstractController
             // J'utilise mon cartService pour vérifier s'il a un panier, si c'est le cas, je le récupère
             if ($user->getRoles()[0] === 'customer') {
                 $cart = $cartService->getCartForUser($user);
-
                 if ($cart === null) { // Si l'utilisateur n'a pas de panier, je crée un nouveau panier
                     $cartService->createCartForUser($user);
                     // $this->addFlash('success', 'Votre panier a été créé avec succès.');
-
                     $this->redirectToRoute('homepage');
                 }
             }
         }
     }
 
-    public function getProductId($idProduct, EntityManagerInterface $em, CartService $cartService)
+    public function handleEventCartForm($idProduct, EntityManagerInterface $em, CartService $cartService)
     {
         $user = $this->getUser();
         if ($user) {
             $product = $em->getRepository(Product::class)->find($idProduct);
             $cartService->addProductToCart($user, $product, 1);
         }
-
-        // Redirigez l'utilisateur vers la page précédente (referer)
+        //Redirect user on the current page
         $referer = $this->requestStack->getCurrentRequest()->headers->get('referer');
         return new RedirectResponse($referer);
     }
