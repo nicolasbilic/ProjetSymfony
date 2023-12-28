15c388199a6f935b883ed08bb88a95ab270e620f
zKittyGB <a-hebert@hotmail.fr>
2023-12-28 11:25:36 +0100

    update list products ariane

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -29,7 +29,7 @@ class OrderController extends AbstractController
         $this->basketController = $basketController;
     }
 
-    #[Route('list', name: 'app_list_orders')]
+    #[Route('list', name: 'app_admin_list_orders')]
     public function list(OrderRepository $orderRepo, Request $request, EntityManagerInterface $em): Response
     {
         $orders = $orderRepo->findAll();
@@ -45,49 +45,13 @@ class OrderController extends AbstractController
                 $em->persist($order);
                 $em->flush();
 
-                return $this->redirectToRoute('app_list_orders');
+                return $this->redirectToRoute('app_admin_list_orders');
             }
         }
 
         return $this->render('orders/listedit.html.twig', [
             'title' => 'Liste des commandes',
             'forms' => $forms,
-        ]);
-    }
-
-    // #[Route('list', name: 'app_list_orders')]
-    // public function list(OrderRepository $orderRepo, Request $request, EntityManagerInterface $em): Response
-    // {
-    //     $orders = $orderRepo->findAll();
-
-    //     return $this->render('orders/listnoedit.html.twig', [
-    //         'title' => 'Liste des commandes',
-    //         'orders' => $orders,
-    //     ]);
-    // }
-
-    #[Route('update/{id}', name: 'app_update_order')]
-    public function update(
-        Request $request,
-        EntityManagerInterface $em,
-        ?Order $order,
-    ) {
-        if ($order === null) {
-            return $this->redirectToRoute('app_list_orders');
-        }
-
-        $form = $this->createForm(OrderFormType::class, $order);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            dump($order);
-            $em->persist($order);
-            $em->flush();
-        }
-        return $this->render('orders/update.html.twig', [
-            'title' => 'Mise à jour de la commande',
-            'form' => $form,
-            'order' => $order,
         ]);
     }
 }
